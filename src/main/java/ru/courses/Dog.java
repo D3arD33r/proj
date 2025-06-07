60f0777c7e7967f21daac8db26708d9e5fb8b28e
D3arD33r <ohh.artyom@gmail.com>
2025-06-07 13:20:12 +0300

    dog work

--- src/main/java/ru/courses/Dog.java
+++ src/main/java/ru/courses/Dog.java
@@ -1,7 +1,17 @@
 package ru.courses;
 
 public class Dog {
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public Dog() {
+    }
+
     public static void main(String[] args) {
         System.out.println("2131231");
+
+
     }
 }
